f4bdb9d6a50f40abdcf3ffbc47cda47f0167b16c
hassen-tahri <75045985+hassen-tahri@users.noreply.github.com>
2022-05-11 02:40:29 +0100

    show dommage Item

--- src/app/pages/constat/show-dommage-item/show-dommage-item.component.ts
+++ src/app/pages/constat/show-dommage-item/show-dommage-item.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NbWindowRef } from '@nebular/theme';
+import { DommageItem } from '../modal-dommage-item/dommage-item';
+import { DommageItemService } from '../modal-dommage-item/dommage-item.service';
 
 @Component({
   selector: 'ngx-show-dommage-item',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowDommageItemComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dommageItemService : DommageItemService,
+    public windowRef: NbWindowRef) { }
 
-  ngOnInit(): void {
+  dommageItem : DommageItem
+  desciption : any
+
+ async ngOnInit() {
+    this.dommageItem = new DommageItem()
+    let id = localStorage.getItem('id');
+    this.dommageItem =  await this.dommageItemService.getById(+id)
+    this.desciption = this.dommageItem.dommage.intitule
+    console.log(this.dommageItem)
+  }
+
+  fermer() {
+    this.windowRef.close();
   }
 
 }
